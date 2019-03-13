2a33658c84291af9bec0463b4a8f625c41307156
minkey <minkey@gmial.com>
2019-03-13 09:59:23 +0800

    先使用java的isReachable,若失败,则尝试使用ping 命令

--- component/src/main/java/com/minkey/command/Ping.java
+++ component/src/main/java/com/minkey/command/Ping.java
@@ -26,7 +26,7 @@ public class Ping {
         boolean status;
         try {
             status = InetAddress.getByName(ipAddress).isReachable(timeout);
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new SystemException(e.getMessage(),e);
         }
         return status;
@@ -41,18 +41,26 @@ public class Ping {
         try {
             return javaPing(ipAddress,DEFALUT_TIMEOUT);
         } catch (SystemException e) {
-            log.debug("ping "+ipAddress +" Exception,"+e.getMessage());
-            return false;
+            log.debug("Java isReachable "+ipAddress +" Exception,"+e.getMessage());
+            try {
+                log.debug("尝试ping命令");
+                //尝试ping命令
+                return pingLinux(ipAddress,1,0.1);
+            } catch (SystemException e2) {
+                log.debug("ping命令执行异常"+e.getMessage());
+                return false;
+            }
         }
     }
 
+
     /**
-     *
-     若line含有=18ms TTL=16字样,说明已经ping通,返回1,否則返回0.
+     * 若line含有=18ms TTL=16字样,说明已经ping通,返回1,否則返回0.
+     *@line "64 bytes from 127.0.0.1: icmp_seq=1 ttl=64 time=0.047 ms"
      */
-    private static int getCheckResult(String line) {
-        // System.out.println("控制台输出的结果为:"+line);
-        Pattern pattern = Pattern.compile("(\\d+ms)(\\s+)(TTL=\\d+)", Pattern.CASE_INSENSITIVE);
+    private static int getCheckResult4Linux(String line) {
+        //计算类似出现=23ms TTL=62字样的次数
+        Pattern pattern = Pattern.compile("(TTL=\\d+)", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(line);
         while (matcher.find()) {
             return 1;
@@ -61,19 +69,20 @@ public class Ping {
     }
 
     /**
-     * windows操作系统ping
+     * linux操作系统ping
      * @param ipAddress
      * @param pingTimes
      * @param timeOut
      * @return
      */
-    public boolean pingWindow(String ipAddress, int pingTimes, int timeOut) {
+    public static boolean pingLinux(String ipAddress, int pingTimes, double timeOut) {
         BufferedReader in = null;
         // 将要执行的ping命令,此命令是windows格式的命令
         Runtime r = Runtime.getRuntime();
-        String pingCommand = "ping " + ipAddress + " -n " + pingTimes + " -w " + timeOut;
-        try {   // 执行命令并获取输出
-            System.out.println(pingCommand);
+        String pingCommand = "ping " + ipAddress + " -c " + pingTimes + " -i " + timeOut;
+        try {
+            // 执行命令并获取输出
+            log.debug("执行ping命令:"+pingCommand);
             Process p = r.exec(pingCommand);
             if (p == null) {
                 return false;
@@ -83,14 +92,15 @@ public class Ping {
             int connectedCount = 0;
             String line = null;
             while ((line = in.readLine()) != null) {
-                connectedCount += getCheckResult(line);
+                log.debug("ping line :"+line);
+                connectedCount += getCheckResult4Linux(line);
             }
+            log.debug("connectedCount="+connectedCount +"|pingTimes="+ pingTimes);
             // 如果出现类似=23ms TTL=62这样的字样,出现的次数=测试次数则返回真
             return connectedCount == pingTimes;
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception e) {
+            throw new SystemException(e.getMessage(),e);
             // 出现异常则返回假
-            return false;
         } finally {
             try {
                 in.close();
@@ -98,6 +108,60 @@ public class Ping {
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * windows操作系统ping
+     * @param ipAddress
+     * @param pingTimes
+     * @param timeOut
+     * @return
+     */
+    public static boolean pingWindow(String ipAddress, int pingTimes, int timeOut) {
+        BufferedReader in = null;
+        // 将要执行的ping命令,此命令是windows格式的命令
+        Runtime r = Runtime.getRuntime();
+        String pingCommand = "ping " + ipAddress + " -n " + pingTimes + " -w " + timeOut;
+        try {
+            // 执行命令并获取输出
+            log.debug("执行ping命令:"+pingCommand);
+            Process p = r.exec(pingCommand);
+            if (p == null) {
+                return false;
+            }
+            // 逐行检查输出,计算类似出现=23ms TTL=62字样的次数
+            in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            int connectedCount = 0;
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                connectedCount += getCheckResult4Windows(line);
+            }
+            // 如果出现类似=23ms TTL=62这样的字样,出现的次数=测试次数则返回真
+            return connectedCount == pingTimes;
+        } catch (Exception ex) {
+            log.error("执行ping异常"+ ex.getMessage());
+            // 出现异常则返回假
+            return false;
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) { }
+        }
+    }
+
+
+    /**
+     *
+     若line含有=18ms TTL=16字样,说明已经ping通,返回1,否則返回0.
+     */
+    private static int getCheckResult4Windows(String line) {
+        //计算类似出现=23ms TTL=62字样的次数
+        Pattern pattern = Pattern.compile("(TTL=\\d+)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(line);
+        while (matcher.find()) {
+            return 1;
+        }
+        return 0;
     }
 
 }
